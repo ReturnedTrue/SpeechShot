06fd7caee5721c961e7c1dc214c0421040d8101e
ReturnedTrue <developerbilly20@gmail.com>
2020-07-14 22:38:10 +0100

    Initial commit

---
+++ index.py
@@ -0,0 +1,42 @@
+from PIL.ImageGrab import grab
+from tkinter.filedialog import asksaveasfilename
+import speech_recognition as speech
+
+typesallowed = [("PNG Image", "*.png"), ("JPEG Image", "*.jpeg"), ("GIF Image", "*.gif")]
+
+while True:
+    recogniser = speech.Recognizer()
+
+    with speech.Microphone() as src:
+        print("Please input:")
+        audio = recogniser.listen(src)
+
+    try:
+        said = recogniser.recognize_google(audio)
+        if (said == "screenshot"):
+            print("Screenshotting...")
+
+            content = grab()
+            filename = asksaveasfilename(filetypes = typesallowed, defaultextension=typesallowed, initialdir="Downloads", title="Save screenshot")
+
+            if (filename):
+                try:
+                    content.save(filename)
+                    print("Saved as: {0}".format(filename))
+                except ValueError:
+                    print("Was unable to save with that file extension.")
+
+            else:
+                print("Chosen not to save file.")
+
+        elif (said == "exit"):
+            print("Exiting...")
+            break
+
+        else:
+            print("You said '{0}' which I don't understand".format(said))
+
+    except speech.UnknownValueError:
+        print("Couldn't understand what you said.")
+    except speech.RequestError as e:
+        print("Couldn't request results from Google Speech Recognition service; {0}".format(e))
